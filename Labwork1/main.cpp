3be69ea0eeb23f724f6c9f9c3e7e5da4f13a54cd
Konstantinst128027 <kostaberezkin403@gmail.com>
2025-06-03 14:20:13 +0300

    I added time-check

--- Labwork1/main.cpp
+++ Labwork1/main.cpp
@@ -1,20 +1,30 @@
 /* Berezkin Konstantin Evgenievich
 ./assignment4 st128027@student.spbu.ru*/
 #include "BMP.h"
+#include <chrono>
 
-int main()
-{
-BMP image_1("file.bmp");
-image_1.Rotate90Clockwise();
-image_1.Save("Rotated90Clockwise.bmp");
+int main() {
+    try {
+        auto start = std::chrono::high_resolution_clock::now();
+        BMP image_1("file.bmp");
+        image_1.Rotate90Clockwise();
+        image_1.Save("Rotated90Clockwise.bmp");
 
-BMP image_2("file.bmp");
-image_2.Rotate90CounterClockwise();
-image_2.Save("Rotate90CounterClockwise.bmp");
+        BMP image_2("file.bmp");
+        image_2.Rotate90CounterClockwise();
+        image_2.Save("Rotate90CounterClockwise.bmp");
 
-BMP image_3("file.bmp");
-image_3.GaussianBlur();
-image_3.Save("GaussianBlur.bmp");
+        BMP image_3("file.bmp");
+        image_3.GaussianBlur();
+        image_3.Save("GaussianBlur.bmp");
+        auto end = std::chrono::high_resolution_clock::now();
+        std::cout << "all process completed in " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms\n";
+    }
+    catch (std::exception& e) {
+        std::cout << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+    
+    return 0;
 
-return 0;
 }
